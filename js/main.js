6cb0118ae2f1cfe9124b564b07d4bf8e4cf5f916
Letian1225 <letian1225@126.com>
2016-02-14 12:24:04 +0800

    browserify, grunt, gulp practice

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ 
+ 
+ var names = require('./names.js');
+ var   findSuperman = require('./findsuperman.js');
+
+if (findSuperman(names())) {
+  document.write('We found Superman');
+} else {
+  document.write('No Superman...');
+}
+ 
+
+ 
---
+++ js/names.js
@@ -0,0 +1,3 @@
+module.exports = function( ) {
+  return ['Barrydf         dddlen', 'Barry Allen', 'Hal Jordan', 'Kara Kent', 'Diana Prince', 'Ray Palmer', 'Oliver Queen', 'Bruce Wayne', 'Wally West', 'John Jones', 'Kyle Rayner', 'Arthur Curry', 'Clark Kent'];
+}
